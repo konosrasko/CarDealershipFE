880e8b3e90f2c37a4e8781baba0300f58547d7d9
atsioplis <atsioplis@ots.gr>
2024-12-16 13:44:17 +0200

    Basic template v2

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,6 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import {MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
