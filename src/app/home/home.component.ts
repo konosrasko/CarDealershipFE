e04c04a4fd3a71a385810a10c271455a7e797624
atsioplis <atsioplis@ots.gr>
2024-12-18 11:41:33 +0200

    some visual changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,6 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: false,
-
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
